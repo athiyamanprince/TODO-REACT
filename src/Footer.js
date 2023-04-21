c50d630a9b2e8f1932681c7c216442a5cd5a70be
athiyamanprince <118097729+athiyamanprince@users.noreply.github.com>
2023-04-21 12:46:53 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <p>Made with ❤ by Athiyaman</p>
+    </footer>
+  );
+}
+
+export default Footer;
